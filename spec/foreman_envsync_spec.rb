3e3f0400c1ab9f7a43cd4162589cfd48f3d75471
Joshua Hoblitt <josh@hoblitt.com>
2021-06-04 10:44:07 -0700

    rm dummy test

--- spec/foreman_envsync_spec.rb
+++ spec/foreman_envsync_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe ForemanEnvsync do
   it "has a version number" do
     expect(ForemanEnvsync::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
