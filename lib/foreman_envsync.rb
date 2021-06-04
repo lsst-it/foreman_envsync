45f9ba5c88833559ec7abe699be4b55cb0a7a2bc
Joshua Hoblitt <josh@hoblitt.com>
2021-06-04 10:39:20 -0700

    generated with bundler 2.2.19

---
+++ lib/foreman_envsync.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "foreman_envsync/version"
+
+module ForemanEnvsync
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/foreman_envsync_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ForemanEnvsync do
+  it "has a version number" do
+    expect(ForemanEnvsync::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
