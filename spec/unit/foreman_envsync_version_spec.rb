10bc152bb2229a949a3a327db6efaba529ff1567
Joshua Hoblitt <josh@hoblitt.com>
2022-09-12 12:23:03 -0700

    re-org ./spec files

--- spec/unit/foreman_envsync_version_spec.rb
+++ spec/unit/foreman_envsync_version_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-RSpec.describe ForemanEnvsync do
+require "spec_helper"
+
+describe ForemanEnvsync do
   it "has a version number" do
     expect(ForemanEnvsync::VERSION).not_to be_nil
   end
