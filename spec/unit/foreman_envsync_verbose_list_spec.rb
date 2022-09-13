1c9012c5f7ca2e4501dc5865f5b8c8531101e8b3
Joshua Hoblitt <josh@hoblitt.com>
2022-09-13 08:37:41 -0700

    fix #verbose_list not appending a new line when items arg is empty

--- spec/unit/foreman_envsync_verbose_list_spec.rb
+++ spec/unit/foreman_envsync_verbose_list_spec.rb
@@ -7,7 +7,7 @@ describe ForemanEnvsync do
     before { @options = { verbose: true } }
 
     context "with array of hash" do
-      let(:aofh) { [{ a: 1 }, { b: 2 }] }
+      let(:items) { [{ a: 1 }, { b: 2 }] }
       let(:foo_output) do
         <<~FOO
           foo
@@ -18,7 +18,18 @@ describe ForemanEnvsync do
         FOO
       end
 
-      it { expect { verbose_list("foo", aofh) }.to output(foo_output).to_stdout }
+      it { expect { verbose_list("foo", items) }.to output(foo_output).to_stdout }
+    end
+
+    context "with empty array" do
+      let(:items) { [] }
+      let(:foo_output) do
+        <<~FOO
+          foo
+        FOO
+      end
+
+      it { expect { verbose_list("foo", items) }.to output(foo_output).to_stdout }
     end
   end
 end
